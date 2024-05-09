48f507c6408224ba22bf5b5aafb5cb7df7d385a5
Victor Martinez <vcrmartinez@gmail.com>
2024-05-09 20:19:28 -0300

    build: rebuild

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,8 @@
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <main>{children}</main>
