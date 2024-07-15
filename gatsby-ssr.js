2115b0e3c58fe9cd2bc367b921b40900b6bb7c95
Victor Martinez <vcrmartinez@gmail.com>
2024-07-15 19:17:09 -0300

    Revert "chore: remove analytics tag"
    This reverts commit 4075d917c572b854356802d09884dae8d6fbd730.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,6 +7,18 @@ const React = require("react")
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `pt-br` })
   setHeadComponents([
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WPNHYC46JG"></script>,
+    <script
+        dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-WPNHYC46JG');
+        `
+        }}
+        />,
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16595958104"></script>,
     <script
         dangerouslySetInnerHTML={{
