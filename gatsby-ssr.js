5b01e4da09bc9d2efb44e71b51c3d951c9ac1ef1
Victor Martinez <vcrmartinez@gmail.com>
2024-09-11 11:10:21 -0300

    chore: update ga4

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,30 +7,11 @@ const React = require("react")
 exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `pt-br` })
   setHeadComponents([
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WPNHYC46JG"></script>,
-    <script
-        dangerouslySetInnerHTML={{
-            __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-WPNHYC46JG');
-        `
-        }}
-        />,
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16595958104"></script>,
-    <script
-        dangerouslySetInnerHTML={{
-          __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'AW-16595958104');
-          `
-        }}
-    />,
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N3V7MRMZ');</script>
   ])
 }
 
